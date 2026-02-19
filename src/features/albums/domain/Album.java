a71e948df1ed60f2cd23821a1884c49e2d6dd9dc
Natalia <natalia.marcos@jcyl.educa.es>
2026-02-19 18:55:31 +0100

    feat: add the atributes, constructor and getters and setters

---
+++ src/features/albums/domain/Album.java
@@ -0,0 +1,39 @@
+package features.albums.domain;
+
+public class Album {
+
+    private String id;
+    private String name;
+    private String publicationYear;
+
+
+    public Album(String id, String name, String publicationYear) {
+        this.id = id;
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(String publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
