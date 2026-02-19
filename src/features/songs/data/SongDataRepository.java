3cc77bc0e42904c9757e96176ce0d14ecc9a1dec
Natalia <natalia.marcos@jcyl.educa.es>
2026-02-19 18:41:23 +0100

    feat: add Song data Repository

---
+++ src/features/songs/data/SongDataRepository.java
@@ -0,0 +1,14 @@
+package features.songs.data;
+
+import features.songs.domain.Song;
+import features.songs.domain.SongRepository;
+
+import java.util.ArrayList;
+
+public class SongDataRepository implements SongRepository {
+
+    @Override
+    public ArrayList<Song> findAll() {
+        return null;
+    }
+}
