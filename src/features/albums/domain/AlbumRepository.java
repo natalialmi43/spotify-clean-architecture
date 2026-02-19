5638995e45da890ec7b9f1d8fe421cfd92c527fb
Natalia <natalia.marcos@jcyl.educa.es>
2026-02-19 18:59:24 +0100

    feat: add the Album Repository

---
+++ src/features/albums/domain/AlbumRepository.java
@@ -0,0 +1,8 @@
+package features.albums.domain;
+
+import java.util.ArrayList;
+
+public interface AlbumRepository {
+
+    public ArrayList <Album> getAlbums ();
+}
--- src/features/albums/domain/albumRepository.java
+++
@@ -1,8 +0,0 @@
-package features.albums.domain;
-
-import java.util.ArrayList;
-
-public interface albumRepository {
-
-    public ArrayList <album> getAlbums ();
-}
